9024fd7287befd2b33f07b79e881961cb9160cf9
hffriman <bgm097@myy.haaga-helia.fi>
2021-03-31 22:25:56 +0300

    First commit

---
+++ src/main/java/com/example/movietheaterapp/MovietheaterappApplication.java
@@ -0,0 +1,13 @@
+package com.example.movietheaterapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovietheaterappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovietheaterappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/movietheaterapp/MovietheaterappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.movietheaterapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovietheaterappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
